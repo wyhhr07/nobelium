0f6ebcf0ecd32c0990cb874e2e67a582fe2b2b2e
wyhhr07 <48158923+wyhhr07@users.noreply.github.com>
2024-09-22 08:20:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: '书目',
+  title: 'BOOKS',
   author: 'Wan',
   email: 'wyhhr07@sina.com',
   link: 'https://book.wanyunhui.top',
@@ -11,7 +11,7 @@ const BLOG = {
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
   darkBackground: '#18181B', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
-  since: , // If leave this empty, current year will be used.
+  since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 20,
   sortByDate: false,
   showAbout: true,
